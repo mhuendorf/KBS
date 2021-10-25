d3dcef62705e4abd7d223fe1bea95ab267b634df
Marcel <mhuendorf@uos.de>
2021-10-25 20:19:16 +0200

    first commit

---
+++ exercises/practical/Assignment_1/main.py
@@ -0,0 +1,17 @@
+import numpy as np
+import panda as pd
+
+
+class DiagnosticProfile:
+    def __init__(self):
+        # list of tuples of
+        # (<attribute value>, <frequency of attribute in conjunction with a diagnose)
+        self.diagnostic_profile = []
+
+
+def exercise():
+    print("test")
+
+
+if __name__ == '__main__':
+    exercise()
