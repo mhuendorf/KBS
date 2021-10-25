2de1920a1ceebc1ffc119fc88e7d433b5a0cace7
Marcel <mhuendorf@uos.de>
2021-10-25 23:50:29 +0200

    added tamplate for the last task

--- exercises/practical/Assignment_1/main.py
+++ exercises/practical/Assignment_1/main.py
@@ -1,12 +1,16 @@
 import numpy as np
-import panda as pd
+import pandas as pd
 
 
 class DiagnosticProfile:
-    def __init__(self):
+    def __init__(self, df: pd.DataFrame, diagnosis: [], findings: []):
         # list of tuples of
         # (<attribute value>, <frequency of attribute in conjunction with a diagnose)
         self.diagnostic_profile = []
+
+    def prune(self, int_val):
+        # do smth
+        self.diagnostic_profile = []  # to be modified in some way
 
 
 def exercise():
