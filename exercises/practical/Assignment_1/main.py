d41f390b2a8054b8ce9a33f91a3515de0cb5803c
Marcel <mhuendorf@uos.de>
2021-10-26 02:36:11 +0200

    added normalization and case counting

--- exercises/practical/Assignment_1/main.py
+++ exercises/practical/Assignment_1/main.py
@@ -7,18 +7,33 @@ class DiagnosticProfile:
 
         # (<attribute value>, <frequency of attribute in conjunction with a diagnose)
         self.diagnostic_profiles = {}
+
+        # initialize diagnostic_profiles structure
         for dia in diagnosis:
             self.diagnostic_profiles[dia] = {}
             for finding in findings:
                 self.diagnostic_profiles[dia][finding] = {}
 
+        # number of cases of every diagnosis
+        self.num_cases = {}
+        # zero cases at the start
+        for diagnose in diagnosis:
+            self.num_cases[diagnose] = 0
+
         for row in df.values:
             dia = row[0]
+            self.num_cases[dia] += 1
             for i in range(1, len(row)):
                 if row[i] in self.diagnostic_profiles[dia][findings[i-1]].keys():
                     self.diagnostic_profiles[dia][findings[i-1]][row[i]] += 1
                 else:
                     self.diagnostic_profiles[dia][findings[i - 1]][row[i]] = 1
+        # normalize values to match frequencies
+        for dia in self.diagnostic_profiles.keys():
+            cases = self.num_cases[dia]
+            for attr in self.diagnostic_profiles[dia]:
+                for attr_val in self.diagnostic_profiles[dia][attr].keys():
+                    self.diagnostic_profiles[dia][attr][attr_val] /= cases
 
         print(self.diagnostic_profiles)
 
