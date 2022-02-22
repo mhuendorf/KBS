45b50b6f1b340682a8524db7ca33f48f199c689c
gvogt <gideonvogt1@gmail.com>
2022-02-22 11:45:57 -0800

    Added datasets with comma and changed main 2

--- term paper/python project/src/main2.py
+++ term paper/python project/src/main2.py
@@ -1,6 +1,7 @@
 from read_write import reader
 import gosdt
 import pandas as pd
+from model.gosdt import GOSDT
 
 if __name__ == '__main__':
     #data = reader.collect_data()
@@ -11,42 +12,22 @@ if __name__ == '__main__':
         print(name + ":")
         print(dataset)
 
-    #with open("../GeneralizedOptimalSparseDecisionTrees-master/test/fixtures/binary_sepal.csv", "r") as data_file:
-    #    data = data_file.read()
-    #with open("../res/spam/spambase.data", "r") as data_file:
-    #    data = data_file.read()
+    dataframe = pd.DataFrame(pd.read_csv("../res/test/monk1-train_comma.csv"))
 
-    #data = pd.read_csv("../res/spam/spambase.data")
-    data = pd.read_csv("../res/mushroom/agaricus-lepiota-Reordered.data")
+    X = dataframe[dataframe.columns[:-1]]
+    y = dataframe[dataframe.columns[-1:]]
 
-    with open("../res/config.json", "r") as config_file:
-        config = config_file.read()
+    hyperparameters = {
+        "regularization": 0.1,
+        "time_limit": 3600,
+        "verbose": True,
+    }
 
-    print("Config:", config)
-    print("Data:", data)
+    model = GOSDT(hyperparameters)
+    model.fit(X, y)
+    print("Execution Time: {}".format(model.time))
 
-    datastring = ""
-
-    for col in data.columns:
-        print(col)
-        #data[col] = pd.cut(data[col],5).astype(str)
-        data[col] = pd.cut(data[col], 5).astype(str)
-
-    for row in range(1,len(data.index)):
-        for col in data.columns:
-            datastring += data[col][row].replace(' ','').replace(',','-') + ","
-        datastring = datastring[:-1]
-        datastring += "\n"
-
-    print("Data:", data)
-    #"""
-    gosdt.configure(config)
-    #datastring2 = data.to_string()
-    print(datastring)
-    result = gosdt.fit(datastring)
-
-    print("Result: ", result)
-    print("Time (seconds): ", gosdt.time())
-    print("Iterations: ", gosdt.iterations())
-    print("Graph Size: ", gosdt.size())
-    #"""
+    prediction = model.predict(X)
+    training_accuracy = model.score(X, y)
+    print("Training Accuracy: {}".format(training_accuracy))
+    print(model.tree)
