625f3c0e732850b74fdfab99762b4702dff24538
gvogt <gideonvogt1@gmail.com>
2022-02-22 08:21:55 -0800

    kind of unnecessary

--- term paper/python project/src/main2.py
+++ term paper/python project/src/main2.py
@@ -1,15 +1,19 @@
 from read_write import reader
 import gosdt
+import pandas as pd
 
 if __name__ == '__main__':
-    data = reader.collect_data()
-    for dataset in data:
-        print(dataset)
+    #data = reader.collect_data()
+    #for dataset in data:
+    #    print(dataset)
 
     #with open("../GeneralizedOptimalSparseDecisionTrees-master/test/fixtures/binary_sepal.csv", "r") as data_file:
     #    data = data_file.read()
-    with open("../res/mushroom/agaricus-lepiota-Reordered.data", "r") as data_file:
-        data = data_file.read()
+    #with open("../res/spam/spambase.data", "r") as data_file:
+    #    data = data_file.read()
+
+    #data = pd.read_csv("../res/spam/spambase.data")
+    data = pd.read_csv("../res/mushroom/agaricus-lepiota-Reordered.data")
 
     with open("../res/config.json", "r") as config_file:
         config = config_file.read()
@@ -17,10 +21,28 @@ if __name__ == '__main__':
     print("Config:", config)
     print("Data:", data)
 
+    datastring = ""
+
+    for col in data.columns:
+        print(col)
+        #data[col] = pd.cut(data[col],5).astype(str)
+        data[col] = pd.cut(data[col], 5).astype(str)
+
+    for row in range(1,len(data.index)):
+        for col in data.columns:
+            datastring += data[col][row].replace(' ','').replace(',','-') + ","
+        datastring = datastring[:-1]
+        datastring += "\n"
+
+    print("Data:", data)
+    #"""
     gosdt.configure(config)
-    result = gosdt.fit(data)
+    #datastring2 = data.to_string()
+    print(datastring)
+    result = gosdt.fit(datastring)
 
     print("Result: ", result)
     print("Time (seconds): ", gosdt.time())
     print("Iterations: ", gosdt.iterations())
     print("Graph Size: ", gosdt.size())
+    #"""
